1877de63a49921a7b54e7ef9f14009617599468e
Mikhail Kondrashin <mkondrashin@gmail.com>
2023-01-17 21:43:12 +0200

    change autorun

--- gmw/autorun/main.go
+++ gmw/autorun/main.go
@@ -2,25 +2,41 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-
-	"golang.org/x/sys/windows/registry"
+	"syscall"
+	"unsafe"
 )
 
 func main() {
-	filePath, err := os.Executable()
-	if err != nil {
-		log.Fatal(err)
+	path, _ := os.Executable()
+	key := syscall.StringToUTF16("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run")
+	value := syscall.StringToUTF16(path)
+	var typ uint32 = 1 //REG_SZ
+	var data []uint16 = value
+
+	_, _, err := syscall.Syscall6(syscall.RegCreateKeyExW,
+		uintptr(syscall.HKEY_CURRENT_USER),
+		uintptr(unsafe.Pointer(&key[0])),
+		0,
+		0,
+		syscall.REG_OPTION_NON_VOLATILE,
+		syscall.KEY_ALL_ACCESS,
+		0)
+	if err != 0 {
+		fmt.Printf("Error creating key: %v\n", err)
+		return
 	}
-	AutoRunPath := `SOFTWARE\Microsoft\Windows\CurrentVersion\Run`
-	k, err := registry.OpenKey(registry.LOCAL_MACHINE, AutoRunPath, registry.WRITE)
-	if err != nil {
-		log.Fatal(err)
+
+	_, _, err = syscall.Syscall6(syscall.RegSetValueExW,
+		uintptr(syscall.HKEY_CURRENT_USER),
+		uintptr(unsafe.Pointer(&key[0])),
+		0,
+		uintptr(typ),
+		uintptr(unsafe.Pointer(&data[0])),
+		uintptr(len(data)))
+	if err != 0 {
+		fmt.Printf("Error setting value: %v\n", err)
+		return
 	}
-	defer k.Close()
-	if err := k.SetStringValue("itachi", filePath); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Autorun added for %s\n", filePath)
+	fmt.Println("%s added to autorun successfully.", path)
 }
